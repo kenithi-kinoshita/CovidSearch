079cc7fde9c2f02bf17ed85b0c2d1b0daa349be9
k-kinoshita <kinoken5939@gmail.com>
2021-12-03 16:37:03 +0900

    Top UI作成

---
+++ CovidSearch/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  CovidSearch
+//
+//  Created by 木下健一 on 2021/12/03.
+//
+
+import UIKit
+
+struct Colors {
+    let bluePurple = UIColor(red: 87/255, green: 99/255, blue: 175/255, alpha: 1)
+    let blue = UIColor(red: 92/255, green: 137/255, blue: 200/255, alpha: 1)
+    
+}

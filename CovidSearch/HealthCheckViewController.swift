7695db9535ba9ed59c9095f9bf6072140131dc8f
k-kinoshita <kinoken5939@gmail.com>
2021-12-07 15:42:14 +0900

    健康管理画面追加

---
+++ CovidSearch/HealthCheckViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HealthCheckViewController.swift
+//  CovidSearch
+//
+//  Created by 木下健一 on 2021/12/07.
+//
+
+import UIKit
+
+class HealthCheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
